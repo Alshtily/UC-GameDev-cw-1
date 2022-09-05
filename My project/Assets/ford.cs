1bc451b78095751be0c270d90b9cf2206c26a456
Saad Almutairi <62450801+Alshtily@users.noreply.github.com>
2022-09-05 19:09:36 +0300

    Done

---
+++ My project/Assets/ford.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class ford : MonoBehaviour
+{
+
+
+    string heroName = "F150";
+    int heroAge = 5;
+    int heroHeight = 187;
+    string heroSuperPower = "ssssssshshshshsshsh";
+
+    string villainNameName = "slva";
+    int villainAge = 2;
+    int villainNameHeight = 177;
+    string villainSuperPower = "baaaaaaaaaaaaa";
+
+
+    
+
+    
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+
+
+    {
+
+        int ageDifference = heroAge - villainAge;
+
+        villainSuperPower = "None";
+
+        heroHeight = heroHeight + 5;
+
+        
+
+
+
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
